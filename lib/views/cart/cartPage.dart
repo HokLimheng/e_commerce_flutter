c370b7e18c19d0b8071180a210c52111f27263b5
HokLimheng <116146514+HokLimheng@users.noreply.github.com>
2024-04-26 20:08:58 +0700

    add new feature in cart

--- lib/views/cart/cartPage.dart
+++ lib/views/cart/cartPage.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce/views/home/models/product.dart';
 import 'package:e_commerce/views/home/widgets/button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 import '../home/widgets/drawer.dart';
@@ -50,108 +51,119 @@ class _CartPageState extends State<CartPage> {
         drawer: const MyDrawer(),
         body: Column(
           children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: value.cart.length,
-                  itemBuilder: (context, index) {
-                    // get product from cart
-                    final Datum cart = value.cart[index];
+           if(value.cart.isEmpty)
+             Expanded(
+               child: Center(
+                 child: Lottie.asset('assets/images/no_cart.json',
+                   width: 200,
+                   height: 200,
+                 ),
+               ),
+             )
+             else
+             Expanded(
+               child: ListView.builder(
+                 itemCount: value.cart.length,
+                 itemBuilder: (context, index) {
+                   // get product from cart
+                   final Datum cart = value.cart[index];
 
-                    final String title = cart.attributes!.title!.toString();
+                   final String title = cart.attributes!.title!.toString();
 
-                    final String price = cart.attributes!.price!.toString();
+                   final String price = cart.attributes!.price!.toString();
 
 
-                    var image = '${CartPage.baseUrl}${cart.attributes!.thumbnail!.data!.attributes!.url!}';
+                   var image = '${CartPage.baseUrl}${cart.attributes!.thumbnail!.data!.attributes!.url!}';
 
-                    return Container(
-                      // padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      margin: const EdgeInsets.only(left: 16, top: 16, right: 16),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
+                   return Container(
+                     // padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                     margin: const EdgeInsets.only(left: 16, top: 16, right: 16),
+                     decoration: BoxDecoration(
+                         color: Colors.white,
+                         borderRadius: BorderRadius.circular(12),
 
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.4),
-                            spreadRadius: 1,
-                            blurRadius: 9,
-                            offset: Offset(0, 3),
-                          ),
-                        ]
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 100,
-                            height: 101,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(image),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                              child: ListTile(
-                                title: Text(
-                                  title,
-                                  style: const TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18
-                                  ),
-                                ),
-                                subtitle: Text(
-                                  '\$ $price',
-                                  style: TextStyle(color: Colors.grey[700]),
-                                ),
-                              ),
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.delete, color: Colors.grey[700]),
-                            onPressed: () {
-                              removeFromCart(cart, context);
-                            },
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-            ),
+                         boxShadow: [
+                           BoxShadow(
+                             color: Colors.grey.withOpacity(0.4),
+                             spreadRadius: 1,
+                             blurRadius: 9,
+                             offset: Offset(0, 3),
+                           ),
+                         ]
+                     ),
+                     child: Row(
+                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                       crossAxisAlignment: CrossAxisAlignment.center,
+                       children: [
+                         Container(
+                           width: 100,
+                           height: 101,
+                           decoration: BoxDecoration(
+                             borderRadius: BorderRadius.circular(12),
+                             image: DecorationImage(
+                               fit: BoxFit.cover,
+                               image: NetworkImage(image),
+                             ),
+                           ),
+                         ),
+                         Expanded(
+                           child: Container(
+                             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                             child: ListTile(
+                               title: Text(
+                                 title,
+                                 style: const TextStyle(
+                                     color: Colors.black,
+                                     fontWeight: FontWeight.bold,
+                                     fontSize: 18
+                                 ),
+                               ),
+                               subtitle: Text(
+                                 '\$ $price',
+                                 style: TextStyle(color: Colors.grey[700]),
+                               ),
+                             ),
+                           ),
+                         ),
+                         IconButton(
+                           icon: Icon(Icons.delete, color: Colors.grey[700]),
+                           onPressed: () {
+                             removeFromCart(cart, context);
+                           },
+                         ),
+                       ],
+                     ),
+                   );
+                 },
+               ),
+             ),
 
-            const Divider(),
+            // Proceed to checkout button
             const SizedBox(height: 2),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey[800],
-                  minimumSize: const Size(203, 48),
-                ),
-                onPressed: () {
-                  // Check if the cart is empty
-                  if (value.cart.isNotEmpty) {
+            if (value.cart.isNotEmpty) // Check if the cart is not empty
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[800],
+                    minimumSize: const Size(203, 48),
+                  ),
+                  onPressed: () {
                     clearCart();
                     Navigator.of(context).push(
                       MaterialPageRoute(builder: (context) => CheckoutPage()),
                     );
-                  }
-                },
-                child: const Text('Proceed to checkout', style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontFamily: 'ProductSans',
-                ),
+                  },
+                  child: const Text(
+                    'Proceed to checkout',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'ProductSans',
+                    ),
+                  ),
                 ),
               ),
-            ),
           ],
         ),
       ),
